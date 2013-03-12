37c0dd2e2a370ff3c130f42fb0fbc063752a3714
Todd Wahnish <t.wahnish@gmail.com>
2013-03-12 13:32:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DynamicForms::Application.config.secret_token = '5e44ceb70b722078f9ea9602de3b3494b2d0ac9b9d23fbaa9691bb4f6c26998a10469945478d120929bd1bdf34db237eaea145a0721656b8f5d32693b7035562'
