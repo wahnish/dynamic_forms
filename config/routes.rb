4da9c969756216eed6dbeaac9e99f6556b15d047
Todd Wahnish <t.wahnish@gmail.com>
2013-03-13 15:48:39 -0400

    built basic functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 DynamicForms::Application.routes.draw do
+  resources :product_types
+
+
+  resources :products
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,7 @@ DynamicForms::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'products#index'
 
   # See how all your routes lay out with "rake routes"
 
